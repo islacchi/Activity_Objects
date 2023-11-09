699b01c78da75a4bb5a36c4e13cc8e45c47b26c8
John Carlo Namor <carlokun1999@gmail.com>
2023-11-09 13:23:42 +0800

    Initial commit

---
+++ info.js
@@ -0,0 +1,48 @@
+export const users = [
+    {
+      nickName:'Loy',
+      name: {
+        firstName:'JOHN CARLO',
+        lastName:'NAMOR',
+      },
+      course:'BSIT',
+      year:'3',
+      
+    },
+    {
+      nickName:'Aming',
+      name: {
+        firstName:'LOLAA',
+        lastName:'FLORAA',
+      },
+      course:'HM',
+      year:'4',
+    },
+    {
+      nickName:'Marjo',
+      name: {
+        firstName:'MARJOLEINE',
+        lastName:'EGAMA',
+      },
+      course:'BSIT',
+      year:'3',
+    },
+    {
+      nickName:'Carlo',
+      name: {
+        firstName:'SOO-HYUN',
+        lastName:'KIM',
+      },
+      course:'IKAW OF COURSE',
+      year:'<3',
+    },
+    {
+      nickName:'Anon',
+      name: {
+        firstName:'Sheesh',
+        lastName:'Eyuh',
+      },
+      course:'BSCS',
+      year:'1',
+    },
+  ];
